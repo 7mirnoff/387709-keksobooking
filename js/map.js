e7ef135f8283b7a07e5595cc7a1ecac155a46200
Dmitrey Smirnov <frontend-manul@yandex.ru>
2018-12-01 21:12:10 +0300

    validation

--- js/map.js
+++ js/map.js
@@ -286,3 +286,106 @@ var openPopup = function (index) {
 };
 
 // КОНЕЦ ** ОТКРЫТИЕ И ЗАКРЫТИЕ ОБЪЯВЛЕНИЙ
+
+// НАЧАЛО ВАЛИДАЦИИ ФОРМЫ
+
+// функция настройки текстового поля ввода
+
+var settingInputText = function (input, type, isRequired, minLength, maxLength) {
+  input.type = type;
+  input.required = isRequired;
+  input.minLength = minLength;
+  input.maxLength = maxLength;
+};
+
+var titleInput = document.querySelector('#title');
+
+settingInputText(titleInput, 'text', true, 30, 100);
+
+// функция настройки числового поля ввода
+
+var settingInputNumber = function (input, type, isRequired, maxValue) {
+  input.type = type;
+  input.required = isRequired;
+  input.max = maxValue;
+};
+
+var priceInput = document.querySelector('#price');
+
+settingInputNumber(priceInput, 'number', true, 1000000);
+
+// функция настройки минимальной цены в зависимости от типа жилья
+
+var selectType = document.querySelector('#type');
+
+var priceRoomMap = {
+  'palace': '10000',
+  'house': '5000',
+  'bungalo': '0',
+  'flat': '1000'
+};
+
+var changeMinPrice = function () {
+
+  var valueSelectedTypeOption = selectType.options[selectType.selectedIndex].value;
+
+  priceInput.placeholder = priceRoomMap[valueSelectedTypeOption];
+  priceInput.min = priceRoomMap[valueSelectedTypeOption];
+};
+
+selectType.addEventListener('change', function () {
+  changeMinPrice();
+});
+
+
+// синхронизаця количества комнат и количества гостей
+
+var selectRooms = document.querySelector('#room_number');
+var selectCapacity = document.querySelector('#capacity');
+
+var sizeRoomMap = {
+  '100': ['0'],
+  '1': ['1'],
+  '2': ['1', '2'],
+  '3': ['1', '2', '3']
+};
+
+var synchronizationRoom = function () {
+  var valueSelectedRoomOption = selectRooms.options[selectRooms.selectedIndex].value;
+  var availableCapacityArr = sizeRoomMap[valueSelectedRoomOption];
+  var optionCapacityArr = selectCapacity.querySelectorAll('option');
+
+  changeDisabled(optionCapacityArr, true);
+
+  for (var m = 0; m < optionCapacityArr.length; m++) {
+    for (var n = 0; n < availableCapacityArr.length; n++) {
+      if (optionCapacityArr[m].value === availableCapacityArr[n]) {
+        optionCapacityArr[m].disabled = false;
+        optionCapacityArr[m].selected = true;
+      }
+    }
+  }
+};
+
+synchronizationRoom();
+
+selectRooms.addEventListener('change', function () {
+  synchronizationRoom();
+});
+
+// синхронизация времени заезда и выезда
+
+var selectTimein = document.querySelector('#timein');
+var selectTimeout = document.querySelector('#timeout');
+
+var synchronizationSelectValue = function (selectRead, selectWrite) {
+  selectWrite.selectedIndex = selectRead.selectedIndex;
+};
+
+selectTimein.addEventListener('change', function () {
+  synchronizationSelectValue(selectTimein, selectTimeout);
+});
+
+selectTimeout.addEventListener('change', function () {
+  synchronizationSelectValue(selectTimeout, selectTimein);
+});
