a099ef89df236de850507bdbc6ca6a21869036bb
Dmitrey Smirnov <frontend-manul@yandex.ru>
2018-12-18 21:51:35 +0300

    вынессоздание функций из инициальзации

--- js/form.js
+++ js/form.js
@@ -2,12 +2,36 @@
 
 (function () {
   var MAX_PRICE = 1000000;
+  var MIN_STRING = 30;
+  var MAX_STRING = 100;
 
   var form = document.querySelector('.ad-form');
   var filters = document.querySelector('.map__filters');
   var resetButton = form.querySelector('.ad-form__reset');
 
   var fieldsetsArr = form.querySelectorAll('.ad-form__element');
+
+  var titleInput = document.querySelector('#title');
+  var priceInput = document.querySelector('#price');
+  var selectType = document.querySelector('#type');
+  var selectRooms = document.querySelector('#room_number');
+  var selectCapacity = document.querySelector('#capacity');
+  var selectTimein = document.querySelector('#timein');
+  var selectTimeout = document.querySelector('#timeout');
+
+  var priceRoomMap = {
+    'palace': '10000',
+    'house': '5000',
+    'bungalo': '0',
+    'flat': '1000'
+  };
+
+  var sizeRoomMap = {
+    '100': ['0'],
+    '1': ['1'],
+    '2': ['1', '2'],
+    '3': ['1', '2', '3']
+  };
 
   var activateForm = function () {
     form.classList.remove('ad-form--disabled');
@@ -23,116 +47,87 @@
     resetButton.addEventListener('click', handler);
   };
 
-  var initForm = function () {
-    var settingInputText = function (input, type, isRequired, minLength, maxLength) {
-      input.type = type;
-      input.required = isRequired;
-      input.minLength = minLength;
-      input.maxLength = maxLength;
-    };
+  var settingInputText = function (input, type, isRequired, minLength, maxLength) {
+    input.type = type;
+    input.required = isRequired;
+    input.minLength = minLength;
+    input.maxLength = maxLength;
+  };
 
-    var titleInput = document.querySelector('#title');
+  var settingInputNumber = function (input, type, isRequired, maxValue) {
+    input.type = type;
+    input.required = isRequired;
+    input.max = maxValue;
+  };
 
-    settingInputText(titleInput, 'text', true, 30, 100);
+  var changeMinPrice = function () {
 
-    var settingInputNumber = function (input, type, isRequired, maxValue) {
-      input.type = type;
-      input.required = isRequired;
-      input.max = maxValue;
-    };
+    var valueSelectedTypeOption = selectType.options[selectType.selectedIndex].value;
 
-    var priceInput = document.querySelector('#price');
+    priceInput.placeholder = priceRoomMap[valueSelectedTypeOption];
+    priceInput.min = priceRoomMap[valueSelectedTypeOption];
+  };
 
-    settingInputNumber(priceInput, 'number', true, MAX_PRICE);
-
-    var selectType = document.querySelector('#type');
-
-    var priceRoomMap = {
-      'palace': '10000',
-      'house': '5000',
-      'bungalo': '0',
-      'flat': '1000'
-    };
-
-    var changeMinPrice = function () {
-
-      var valueSelectedTypeOption = selectType.options[selectType.selectedIndex].value;
-
-      priceInput.placeholder = priceRoomMap[valueSelectedTypeOption];
-      priceInput.min = priceRoomMap[valueSelectedTypeOption];
-    };
-
+  selectType.addEventListener('change', function () {
     changeMinPrice();
+  });
 
-    selectType.addEventListener('change', function () {
-      changeMinPrice();
-    });
+  var synchronizationRoom = function () {
+    var valueSelectedRoomOption = selectRooms.options[selectRooms.selectedIndex].value;
+    var availableCapacityArr = sizeRoomMap[valueSelectedRoomOption];
+    var optionCapacityArr = selectCapacity.querySelectorAll('option');
 
-    var selectRooms = document.querySelector('#room_number');
-    var selectCapacity = document.querySelector('#capacity');
+    window.util.changeDisabled(optionCapacityArr, true);
 
-    var sizeRoomMap = {
-      '100': ['0'],
-      '1': ['1'],
-      '2': ['1', '2'],
-      '3': ['1', '2', '3']
-    };
-
-    var synchronizationRoom = function () {
-      var valueSelectedRoomOption = selectRooms.options[selectRooms.selectedIndex].value;
-      var availableCapacityArr = sizeRoomMap[valueSelectedRoomOption];
-      var optionCapacityArr = selectCapacity.querySelectorAll('option');
-
-      window.util.changeDisabled(optionCapacityArr, true);
-
-      for (var m = 0; m < optionCapacityArr.length; m++) {
-        for (var n = 0; n < availableCapacityArr.length; n++) {
-          if (optionCapacityArr[m].value === availableCapacityArr[n]) {
-            optionCapacityArr[m].disabled = false;
-            optionCapacityArr[m].selected = true;
-          }
+    for (var m = 0; m < optionCapacityArr.length; m++) {
+      for (var n = 0; n < availableCapacityArr.length; n++) {
+        if (optionCapacityArr[m].value === availableCapacityArr[n]) {
+          optionCapacityArr[m].disabled = false;
+          optionCapacityArr[m].selected = true;
         }
       }
-    };
+    }
+  };
+
+  selectRooms.addEventListener('change', function () {
+    synchronizationRoom();
+  });
+
+  var synchronizationSelectValue = function (selectRead, selectWrite) {
+    selectWrite.selectedIndex = selectRead.selectedIndex;
+  };
+
+  selectTimein.addEventListener('change', function () {
+    synchronizationSelectValue(selectTimein, selectTimeout);
+  });
+
+  selectTimeout.addEventListener('change', function () {
+    synchronizationSelectValue(selectTimeout, selectTimein);
+  });
+
+  form.addEventListener('submit', function (evt) {
+    window.backend.upload(new FormData(form), window.massegeForm.onLoad, window.massegeForm.onError);
+    evt.preventDefault();
+  });
+
+  resetButton.addEventListener('click', function (evtReset) {
+    evtReset.preventDefault();
+    form.reset();
+    filters.reset();
+
+    window.setAdress();
 
     synchronizationRoom();
+    changeMinPrice();
 
-    selectRooms.addEventListener('change', function () {
-      synchronizationRoom();
-    });
+    window.util.isFirstMove = true;
+  });
 
-    var selectTimein = document.querySelector('#timein');
-    var selectTimeout = document.querySelector('#timeout');
-
-    var synchronizationSelectValue = function (selectRead, selectWrite) {
-      selectWrite.selectedIndex = selectRead.selectedIndex;
-    };
-
-    selectTimein.addEventListener('change', function () {
-      synchronizationSelectValue(selectTimein, selectTimeout);
-    });
-
-    selectTimeout.addEventListener('change', function () {
-      synchronizationSelectValue(selectTimeout, selectTimein);
-    });
-
-    form.addEventListener('submit', function (evt) {
-      window.backend.upload(new FormData(form), window.massegeForm.onLoad, window.massegeForm.onError);
-      evt.preventDefault();
-    });
-
-    resetButton.addEventListener('click', function (evtReset) {
-      evtReset.preventDefault();
-      form.reset();
-      filters.reset();
-
-      window.setAdress();
-
-      synchronizationRoom();
-      changeMinPrice();
-
-      window.util.isFirstMove = true;
-    });
+  var initForm = function () {
+    settingInputText(titleInput, 'text', true, MIN_STRING, MAX_STRING);
+    settingInputNumber(priceInput, 'number', true, MAX_PRICE);
+    changeMinPrice();
+    synchronizationRoom();
   };
 
   window.form = {
